c3c27c5927dd8e4f8a434603479001c61b748a28
Steddy <138579152+Steddyz@users.noreply.github.com>
2024-02-11 12:18:20 +0300

    first commit
    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const keys = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+const getRandomNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const getRandomKey = () => {
+  return keys[getRandomNumber(0, keys.length - 1)];
+};
+
+const targetRandomKey = () => {
+  const key = document.getElementById(getRandomKey());
+  const keys = document.querySelectorAll(".keyboard ul li");
+
+  keys.forEach((key) => {
+    key.classList.remove("selected");
+    key.classList.remove("hit");
+  });
+
+  key.classList.add("selected");
+};
+
+let score = 0;
+
+document.addEventListener("keyup", (event) => {
+  const keyPressed = String.fromCharCode(event.keyCode);
+  const keyElement = document.getElementById(keyPressed);
+  const hightlightKey = document.querySelector(".selected");
+  const result = document.querySelector(".result");
+
+  keyElement.classList.add("hit");
+  keyElement.addEventListener("animationend", () => {
+    keyElement.classList.remove("hit");
+
+    if (keyPressed === hightlightKey.id) {
+      score = score + 1;
+    } else {
+      alert("You've lost, so click carefully.");
+      score = 0;
+    }
+
+    result.innerHTML = `Score: ${score}`;
+
+    hightlightKey.classList.remove("selected");
+    targetRandomKey();
+  });
+});
+
+targetRandomKey();
